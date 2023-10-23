908010605cae34399fdf04732c4adfff8c431a57
Vishaal Karthik Muralidharan <vmural9@uic.edu>
2023-10-23 11:37:29 -0500

    Update register.ts

--- src/pages/api/auth/register.ts
+++ src/pages/api/auth/register.ts
@@ -11,6 +11,11 @@ export const POST: APIRoute = async ({ request, redirect }) => {
   const password = formData.get("password");
   const displayName = formData.get("name");
 
+  console.log(email);
+  console.log(password);
+  console.log(displayName);
+  console.log(typeof email);
+
   // Check for missing fields and return a 400 response if any are missing
   if (!email || !password || !displayName) {
     return new Response("Missing registration fields", {
@@ -19,12 +24,21 @@ export const POST: APIRoute = async ({ request, redirect }) => {
   }
   try {
     // Create a new user using Firebase Auth service
-    await auth.createUser({
-      email: email,
-      password: password,
-      displayName: displayName,
-    });
-    console.log("Creating User.");
+    await auth
+      .createUser({
+        email: email,
+        password: password,
+        displayName: displayName,
+      })
+      .then((userRecord) => {
+        console.log("Successfully created new user:", userRecord.uid);
+      })
+      .catch((error) => {
+        console.log("Error creating new user:", error);
+      });
+
+    console.log(email);
+    console.log("Creating User");
     // If successful, you can redirect to /signin or return a success message
     // For the purpose of this example, we'll return a success message.
     // In a real-world application, you'd typically redirect the user.
@@ -32,7 +46,7 @@ export const POST: APIRoute = async ({ request, redirect }) => {
     return redirect("/signin");
   } catch (error) {
     console.log(error);
-    return new Response(`Something went wrong`, {
+    return new Response(`Something went wrong lololol`, {
       status: 400,
     });
   }
